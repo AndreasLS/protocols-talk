6552d8f969537d6a7819233cc1157eb81634ea75
André Salla <andresalla@Andres-MacBook-Pro.local>
2022-02-20 23:13:03 -0300

    Updated presentation

--- Protocols.playground/Sources/AssociatedTypes.swift
+++ Protocols.playground/Sources/AssociatedTypes.swift
@@ -26,6 +26,7 @@ public typealias ReadAndWriteRepository = ReadRepository & WriteRepository
 
 public enum UserDefaultsError: Error {
     case noRecordFound
+    case parseError
 }
 
 public extension ReadRepository where Entity: Codable {
@@ -49,6 +50,56 @@ public extension WriteRepository where Entity: Codable {
         let string = String(data: data, encoding: .utf8)
         UserDefaults.standard.set(string, forKey: "myDummyDatabase")
         completion(true)
+    }
+}
+
+protocol JSONReadWriteRepositoryProtocol: ReadAndWriteRepository where Entity: Codable { }
+
+extension JSONReadWriteRepositoryProtocol {
+    
+    func write(values: Entity, completion: @escaping (Bool) -> Void) throws {
+        let data = try JSONEncoder().encode(values)
+        let string = String(data: data, encoding: .utf8)
+        UserDefaults.standard.set(string, forKey: "myDummyDatabase")
+        completion(true)
+    }
+    
+    func read(_ completion: @escaping (Entity)->Void) throws {
+        let string = UserDefaults.standard.string(forKey: "myDummyDatabase")
+        if let data = string?.data(using: .utf8) {
+            let object = try JSONDecoder().decode(Entity.self, from: data)
+            completion(object)
+        } else {
+            throw UserDefaultsError.noRecordFound
+        }
+    }
+}
+
+protocol DictionaryReadWriteRepositoryProtocol:
+    ReadAndWriteRepository where Entity == [String: Any] { }
+
+extension DictionaryReadWriteRepositoryProtocol {
+    
+    func write(values: Entity, completion: @escaping (Bool) -> Void) throws {
+        let data = try JSONSerialization.data(withJSONObject: values,
+                                              options: .fragmentsAllowed)
+        let string = String(data: data, encoding: .utf8)
+        UserDefaults.standard.set(string, forKey: "myDummyDatabase")
+        completion(true)
+    }
+    
+    func read(_ completion: @escaping (Entity)->Void) throws {
+        guard let string = UserDefaults.standard.string(forKey: "myDummyDatabase") else {
+            throw UserDefaultsError.noRecordFound
+        }
+        if let data = string.data(using: .utf8),
+            let object = try JSONSerialization
+                .jsonObject(
+                    with: data, options: .fragmentsAllowed) as? [String: Any] {
+            completion(object)
+        } else {
+            throw UserDefaultsError.parseError
+        }
     }
 }
 
